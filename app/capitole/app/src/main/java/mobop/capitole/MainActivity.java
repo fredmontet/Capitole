7548379cf4e3771b87f249fe5b3cad6b0e618651
fredmontet <hello@fredmontet.com>
2015-11-04 22:15:24 +0100

    Tabs v 0.1

--- app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
+++ app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
@@ -1,16 +1,51 @@
 package mobop.capitole;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//import mobop.capitole.tab.TabsPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    //TabsPagerAdapter mTabsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+
+    ViewPager mViewPager;
+    FragmentPagerAdapter adapterViewPager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        //mTabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), getApplicationContext());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        adapterViewPager = new TabsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(adapterViewPager);
     }
 
     @Override
@@ -34,4 +69,48 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * Inner class for the Tabs on main activity
+     */
+    public static class TabsPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public TabsPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return FilmFragment.newInstance(0);
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return FilmFragment.newInstance(1);
+                case 2: // Fragment # 1 - This will show SecondFragment
+                    return FilmFragment.newInstance(2);
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+
+    }
 }
+
+
+
+
