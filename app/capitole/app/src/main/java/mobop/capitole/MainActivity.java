2be40b623d0e1cbb59932a5790f8825e2bd37d56
fredmontet <hello@fredmontet.com>
2015-11-04 22:55:57 +0100

    Project clean

--- app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
+++ app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
@@ -9,19 +9,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//import mobop.capitole.tab.TabsPagerAdapter;
-
+/**
+ * The main activity
+ */
 public class MainActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    //TabsPagerAdapter mTabsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -31,16 +22,10 @@ public class MainActivity extends AppCompatActivity {
     FragmentPagerAdapter adapterViewPager;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        //mTabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), getApplicationContext());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -57,12 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
