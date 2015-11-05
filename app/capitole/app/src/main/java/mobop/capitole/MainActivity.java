7c819b30e6d6c1483d52ee8fa1cff22a934c7a6a
fredmontet <hello@fredmontet.com>
2015-11-05 23:49:07 +0100

    Revert "ListView implementation failed"
    This reverts commit 6bd17e17b0306bef58116bde57e7b1cc3d840e67.

--- app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
+++ app/capitole/app/src/main/java/mobop/capitole/MainActivity.java
@@ -1,6 +1,5 @@
 package mobop.capitole;
 
-import android.app.ListFragment;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,26 +7,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.List;
-
-
-import java.util.List;
 
 /**
  * The main activity
@@ -49,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
-        adapterViewPager = new ListViewDemoAdapter(getSupportFragmentManager(), getApplicationContext());
+        adapterViewPager = new TabsPagerAdapter(getSupportFragmentManager(), getApplicationContext());
         mViewPager.setAdapter(adapterViewPager);
     }
 
@@ -76,50 +57,51 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Inner class for the Tabs on main activity
      */
-    public class ListViewDemoAdapter extends ArrayAdapter<ListViewItem> {
+    public static class TabsPagerAdapter extends FragmentPagerAdapter {
+        protected Context mContext;
+        private static int NUM_ITEMS = 3;
 
-        public ListViewDemoAdapter(Context context, List<ListViewItem> items) {
-            super(context, R.layout.listview_item, items);
+        public TabsPagerAdapter(FragmentManager fragmentManager, Context context) {
+            super(fragmentManager);
+            this.mContext = context;
         }
 
+        // Returns total number of pages
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder viewHolder;
+        public int getCount() {
+            return NUM_ITEMS;
+        }
 
-            if(convertView == null) {
-                // inflate the GridView item layout
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView = inflater.inflate(R.layout.listview_item, parent, false);
-
-                // initialize the view holder
-                viewHolder = new ViewHolder();
-                viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-                viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
-                convertView.setTag(viewHolder);
-            } else {
-                // recycle the already inflated view
-                viewHolder = (ViewHolder) convertView.getTag();
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return FilmFragment.newInstance(0);
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return FilmFragment.newInstance(1);
+                case 2: // Fragment # 1 - This will show SecondFragment
+                    return FilmFragment.newInstance(2);
+                default:
+                    return null;
             }
-
-            // update the item view
-            ListViewItem item = getItem(position);
-            viewHolder.tvTitle.setText(item.title);
-            viewHolder.tvDescription.setText(item.description);
-
-            return convertView;
         }
 
-        /**
-         * The view holder design pattern prevents using findViewById()
-         * repeatedly in the getView() method of the adapter.
-         *
-         * @see //http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
-         */
-        private static class ViewHolder {
-            ImageView ivIcon;
-            TextView tvTitle;
-            TextView tvDescription;
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return mContext.getResources().getString(R.string.tab_seen);
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return mContext.getResources().getString(R.string.tab_suggestion);
+                case 2: // Fragment # 1 - This will show SecondFragment
+                    return mContext.getResources().getString(R.string.tab_tosee);
+                default:
+                    return null;
+            }
         }
+
     }
 }
 
