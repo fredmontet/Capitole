5ef92395ce14c76b07447785d022534905c9a431
fredmontet <hello@fredmontet.com>
2016-01-01 16:19:44 +0100

    movieManager comment

--- app/capitole/app/src/main/java/mobop/capitole/domain/MovieManager.java
+++ app/capitole/app/src/main/java/mobop/capitole/domain/MovieManager.java
@@ -13,31 +13,34 @@ import java.util.List;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
-import io.realm.RealmResults;
-import mobop.capitole.Capitole;
-
 import mobop.capitole.domain.mapper.tmdb.MovieJsonMapper;
 import mobop.capitole.domain.model.Movie;
 import mobop.capitole.domain.model.User;
 import mobop.capitole.domain.net.ApiQuery;
 
-
-public class MovieManager extends Application{
+/**
+ * This is the main controller of the Capitole app
+ * <p/>
+ * It manages all the user requests and different
+ * interactions with the model and the API resources
+ */
+public class MovieManager extends Application {
 
     private Realm realm;
     private User user;
     private Context context;
     private MovieJsonMapper movieJsonMapper;
 
-
-    // Constructor
-    //============
-
-    public MovieManager(Context context, User user){
+    /**
+     * Constructor
+     *
+     * @param context
+     * @param user
+     */
+    public MovieManager(Context context, User user) {
         this.user = user;
         this.context = context;
         this.movieJsonMapper = new MovieJsonMapper();
-
 
         // Configure Realm
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this.context)
@@ -53,9 +56,17 @@ public class MovieManager extends Application{
         this.realm = Realm.getInstance(realmConfiguration);
     }
 
-    // API Functions
-    //==============
 
+    // API Functions
+    //==============================================================================================
+
+    /**
+     * Get a movie details by tmdbId from themoviedb.org
+     *
+     * @param tmdbId   The id of the movie
+     * @param callback The callback containing the response
+     * @throws MalformedURLException
+     */
     public void getMovieFromApi(String tmdbId, final MovieManagerCallback callback) throws MalformedURLException {
         ApiQuery api = new ApiQuery(this.context);
         api.getMovieById(tmdbId, new ApiQuery.ApiQueryCallback() {
@@ -74,6 +85,13 @@ public class MovieManager extends Application{
         });
     }
 
+    /**
+     * Query the tmdb database for a list of movies given from a query by title
+     *
+     * @param query    The title of the film to look for
+     * @param callback The callback containing the response
+     * @throws MalformedURLException
+     */
     public void searchMovieFromApi(String query, final MovieManagerCallback callback) throws MalformedURLException {
         ApiQuery api = new ApiQuery(this.context);
         api.getMoviesByTitle(query, new ApiQuery.ApiQueryCallback() {
@@ -92,6 +110,12 @@ public class MovieManager extends Application{
         });
     }
 
+    /**
+     * Get some general movie suggestions from themoviedb.org
+     *
+     * @param callback The callback containing the response
+     * @throws MalformedURLException
+     */
     public void getSuggestion(final MovieManagerCallback callback) throws MalformedURLException {
         ApiQuery api = new ApiQuery(this.context);
         api.getMovieSuggestion(new ApiQuery.ApiQueryCallback() {
@@ -111,14 +135,28 @@ public class MovieManager extends Application{
 
     }
 
-    // Local Functions
-    //================
 
-    public Movie getMovie(String movieId){
+    // Local Functions
+    //==============================================================================================
+
+    /**
+     * Get a movie by its ID
+     *
+     * @param movieId the Capitole UUID of a movie
+     * @return The movie object
+     */
+    public Movie getMovie(String movieId) {
         return this.realm.where(Movie.class).equalTo("uuid", movieId).findFirst();
     }
 
-    public void rateMovie(Movie movie, float rating, String comment){
+    /**
+     * Rate a movie
+     *
+     * @param movie   The movie to rate
+     * @param rating  The rating to give
+     * @param comment The String of the comment
+     */
+    public void rateMovie(Movie movie, float rating, String comment) {
         this.realm.beginTransaction();
         movie.setRating(rating);
         movie.setComment(comment);
@@ -126,35 +164,51 @@ public class MovieManager extends Application{
         this.realm.commitTransaction();
     }
 
-    public List<Movie> getMoviesSeen(){
+    /**
+     * Get the list of seen movies
+     *
+     * @return The list of seen movies
+     */
+    public List<Movie> getMoviesSeen() {
         return this.user.getMoviesSeen();
     }
 
-    public List<Movie> getMoviesToSee(){
+    /**
+     * Get the list of movies to see
+     *
+     * @return The list of movies to see
+     */
+    public List<Movie> getMoviesToSee() {
         return this.user.getMoviesToSee();
     }
 
-    public boolean addToMoviesSeen(Movie movie){
+    /**
+     * Add a given movie to the list of movies seen
+     *
+     * @param movie The movie to add
+     * @return boolean false if the movie is already in one of the two lists (Seen and To See)
+     */
+    public boolean addToMoviesSeen(Movie movie) {
 
         boolean inToSeeList = false;
-        for(Movie movieToSee : this.user.getMoviesToSee()) {
-            if(movieToSee.getTmdbID().equals(movie.getTmdbID())){
+        for (Movie movieToSee : this.user.getMoviesToSee()) {
+            if (movieToSee.getTmdbID().equals(movie.getTmdbID())) {
                 inToSeeList = true;
                 break;
             }
         }
 
         boolean inSeenList = false;
-        for(Movie movieSeen : this.user.getMoviesSeen()) {
-            if(movieSeen.getTmdbID().equals(movie.getTmdbID())){
+        for (Movie movieSeen : this.user.getMoviesSeen()) {
+            if (movieSeen.getTmdbID().equals(movie.getTmdbID())) {
                 inSeenList = true;
                 break;
             }
         }
 
-        if(inToSeeList || inSeenList){
+        if (inToSeeList || inSeenList) {
             return false;
-        }else{
+        } else {
             this.realm.beginTransaction();
             this.user.getMoviesSeen().add(movie);
             realm.copyToRealmOrUpdate(movie);
@@ -163,27 +217,33 @@ public class MovieManager extends Application{
         }
     }
 
-    public boolean addToMoviesToSee(Movie movie){
+    /**
+     * Add a given movie to the list of movies to see
+     *
+     * @param movie The movie to add
+     * @return boolean false if the movie is already in one of the two lists (Seen and To See)
+     */
+    public boolean addToMoviesToSee(Movie movie) {
 
         boolean inToSeeList = false;
-        for(Movie movieToSee : this.user.getMoviesToSee()) {
-            if(movieToSee.getTmdbID().equals(movie.getTmdbID())){
+        for (Movie movieToSee : this.user.getMoviesToSee()) {
+            if (movieToSee.getTmdbID().equals(movie.getTmdbID())) {
                 inToSeeList = true;
                 break;
             }
         }
 
         boolean inSeenList = false;
-        for(Movie movieSeen : this.user.getMoviesSeen()) {
-            if(movieSeen.getTmdbID().equals(movie.getTmdbID())){
+        for (Movie movieSeen : this.user.getMoviesSeen()) {
+            if (movieSeen.getTmdbID().equals(movie.getTmdbID())) {
                 inSeenList = true;
                 break;
             }
         }
 
-        if(inToSeeList || inSeenList){
+        if (inToSeeList || inSeenList) {
             return false;
-        }else{
+        } else {
             this.realm.beginTransaction();
             this.user.getMoviesToSee().add(movie);
             realm.copyToRealmOrUpdate(movie);
@@ -192,7 +252,13 @@ public class MovieManager extends Application{
         }
     }
 
-    public boolean removeFromMoviesSeen(Movie movie){
+    /**
+     * Removes a movie from the list of seen movies
+     *
+     * @param movie The movie to remove
+     * @return {@code true} if the movie has been removed, {@code false} otherwise.
+     */
+    public boolean removeFromMoviesSeen(Movie movie) {
         realm.beginTransaction();
         boolean res = this.user.getMoviesSeen().remove(movie);
         movie.removeFromRealm();
@@ -200,7 +266,13 @@ public class MovieManager extends Application{
         return res;
     }
 
-    public boolean removeFromMoviesToSee(Movie movie){
+    /**
+     * Removes a movie from the list of movies to see
+     *
+     * @param movie The movie to remove
+     * @return {@code true} if the movie has been removed, {@code false} otherwise.
+     */
+    public boolean removeFromMoviesToSee(Movie movie) {
         realm.beginTransaction();
         boolean res = this.user.getMoviesToSee().remove(movie);
         movie.removeFromRealm();
@@ -208,14 +280,21 @@ public class MovieManager extends Application{
         return res;
     }
 
-    public void changeList(Movie movie){
-        if(this.user.getMoviesToSee().contains(movie)){
+
+    /**
+     * Change a movie from the list to see to the list seen
+     * or the other way depending in which list the movie is in.
+     *
+     * @param movie The movie to move
+     */
+    public void changeList(Movie movie) {
+        if (this.user.getMoviesToSee().contains(movie)) {
             realm.beginTransaction();
             this.user.getMoviesSeen().add(movie);
             this.user.getMoviesToSee().remove(movie);
             realm.copyToRealmOrUpdate(movie);
             realm.commitTransaction();
-        }else if(this.user.getMoviesSeen().contains(movie)){
+        } else if (this.user.getMoviesSeen().contains(movie)) {
             realm.beginTransaction();
             this.user.getMoviesToSee().add(movie);
             this.user.getMoviesSeen().remove(movie);
@@ -224,19 +303,18 @@ public class MovieManager extends Application{
         }
     }
 
-
     /**
      * Function to close the MovieManager
-     * In fact, it just close the realm...
+     * In fact, it just closes the realm...
      */
-    public void close(){
+    public void close() {
         this.realm.close(); // Remember to close Realm when done.
     }
 
     /**
-     * Callback interface
+     * Simple Callback interface
      */
-    public interface MovieManagerCallback{
+    public interface MovieManagerCallback {
         void onSuccess(List<Movie> response);
     }
 
